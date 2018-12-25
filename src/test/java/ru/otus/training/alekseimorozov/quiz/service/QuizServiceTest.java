1af28a003fac73a4768013227d1070eb9ab99f2f
Morozov Aleksey <a.m.morozov@gmail.com>
2018-12-25 15:06:53 +0300

    task04 quiz with spring sell and fixes

--- src/test/java/ru/otus/training/alekseimorozov/quiz/service/QuizServiceTest.java
+++ src/test/java/ru/otus/training/alekseimorozov/quiz/service/QuizServiceTest.java
@@ -7,13 +7,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.otus.training.alekseimorozov.quiz.dao.QuestionDao;
 
 import java.io.IOException;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(properties = {
+        ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false",
+        InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false"
+})
 public class QuizServiceTest {
     @MockBean
     private QuestionDao questionDao;
